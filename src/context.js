9ca89ef0b3c93d5f9b6ac0c4015361d5d0d2282b
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-11-16 16:58:12 +0530

    added clearCart action creator

--- src/context.js
+++ src/context.js
@@ -1,34 +1,39 @@
-import React, { useState, useContext, useReducer, useEffect } from 'react'
-import cartItems from './data'
-import reducer from './reducer'
+import React, { useState, useContext, useReducer, useEffect } from "react";
+import cartItems from "./data";
+import reducer from "./reducer";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-useReducer-cart-project'
+const url = "https://course-api.com/react-useReducer-cart-project";
 
-const AppContext = React.createContext()
+const AppContext = React.createContext();
 const initialState = {
   loading: false,
   cart: cartItems,
   total: 0,
-  amount: 0
-}
+  amount: 0,
+};
 
 const AppProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer,initialState)
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
 
   return (
     <AppContext.Provider
       value={{
-        ...state
+        ...state,
+        clearCart
       }}
     >
       {children}
     </AppContext.Provider>
-  )
-}
+  );
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
