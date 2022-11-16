39e16a27a9f713c490b559d1725f9d0e380f5975
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-11-16 16:07:35 +0530

    initial render

--- src/context.js
+++ src/context.js
@@ -4,15 +4,22 @@ import reducer from './reducer'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-useReducer-cart-project'
+
 const AppContext = React.createContext()
+const initialState = {
+  loading: false,
+  cart: cartItems,
+  total: 0,
+  amount: 0
+}
 
 const AppProvider = ({ children }) => {
-  const [cart, setCart] = useState(cartItems)
+  const [state, dispatch] = useReducer(reducer,initialState)
 
   return (
     <AppContext.Provider
       value={{
-        cart,
+        ...state
       }}
     >
       {children}
