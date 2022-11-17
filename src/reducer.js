6a4b1d91628a283f1b205c67dd7431d2983f315b
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-11-17 17:02:17 +0530

    implemented remove item from cart functionality

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,11 @@
 const reducer = (state, action) => {
+    if (action.type === 'CLEAR_CART') {
+        return { ...state, cart: [] }
+    }
+    if (action.type === 'REMOVE') {
+        const newCart = state.cart.filter(ele => ele.id !== action.payload)
+        return { ...state, cart: newCart }
+    }
     return state;
 }
 
