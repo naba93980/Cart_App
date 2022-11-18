bb122c114d84dcf4ca6a2a9af78d1523a4f09acd
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-11-18 15:10:13 +0530

    increase and decrease item amount functionality added

--- src/reducer.js
+++ src/reducer.js
@@ -6,6 +6,25 @@ const reducer = (state, action) => {
         const newCart = state.cart.filter(ele => ele.id !== action.payload)
         return { ...state, cart: newCart }
     }
+    if (action.type === 'INCREASE') {
+        let tempCart = state.cart.map((cartItem) => {
+            if (cartItem.id === action.payload) {
+                return { ...cartItem, amount : cartItem.amount + 1 };
+            }
+            return cartItem;
+        })
+        return { ...state, cart: tempCart };
+    }
+    if (action.type === 'DECREASE') {
+        let tempCart = state.cart.map((cartItem) => {
+            if (cartItem.id === action.payload) {
+                if (cartItem.amount===0) return cartItem
+                return { ...cartItem, amount : cartItem.amount - 1 };
+            }
+            return cartItem;
+        })
+        return { ...state, cart: tempCart };
+    }
     return state;
 }
 
