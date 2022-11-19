8717e97ee911f6609220cc55698dd4cfad317e57
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-11-19 23:50:42 +0530

    implemented increase decrease cart item count and fetchdata functionality

--- src/context.js
+++ src/context.js
@@ -22,14 +22,39 @@ const AppProvider = ({ children }) => {
 
   const remove = (id) => {
     dispatch({ type: "REMOVE", payload: id });
+  };
+
+  const increase = (id) => {
+    dispatch({ type: "INCREASE", payload: id });
+  };
+
+  const decrease = (id) => {
+    dispatch({ type: "DECREASE", payload: id });
+  };
+
+  const fetchData = async () => {
+    dispatch({ type: 'LOADING' });
+    const response = await fetch(url);
+    const cart = await response.json();
+    dispatch({ type: 'DISPLAY_ITEMS', payload: cart });
   }
+
+  useEffect(() => {
+    dispatch({ type: "GET_TOTALS" });
+  }, [state.cart]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <AppContext.Provider
       value={{
         ...state,
         clearCart,
-        remove
+        remove,
+        increase,
+        decrease,
       }}
     >
       {children}
