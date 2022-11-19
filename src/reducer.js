85747011729b1ad40e2d1d67b51b4ab10b411530
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-11-19 23:48:26 +0530

    implemented getTotal, loading and displayitem action type

--- src/reducer.js
+++ src/reducer.js
@@ -9,7 +9,7 @@ const reducer = (state, action) => {
     if (action.type === 'INCREASE') {
         let tempCart = state.cart.map((cartItem) => {
             if (cartItem.id === action.payload) {
-                return { ...cartItem, amount : cartItem.amount + 1 };
+                return { ...cartItem, amount: cartItem.amount + 1 };
             }
             return cartItem;
         })
@@ -18,12 +18,30 @@ const reducer = (state, action) => {
     if (action.type === 'DECREASE') {
         let tempCart = state.cart.map((cartItem) => {
             if (cartItem.id === action.payload) {
-                if (cartItem.amount===0) return cartItem
-                return { ...cartItem, amount : cartItem.amount - 1 };
+                return { ...cartItem, amount: cartItem.amount - 1 };
             }
             return cartItem;
-        })
+        }).filter((cartItem) => cartItem.amount !== 0)
         return { ...state, cart: tempCart };
+    }
+    if (action.type === 'GET_TOTALS') {
+        let { total, amount } = state.cart.reduce((cartTotal, cartItem) => {
+            const { price, amount } = cartItem;
+            cartTotal.amount += amount;
+            cartTotal.total += price * amount;
+            return cartTotal;
+        }, {
+            total: 0,
+            amount: 0,
+        })
+        total = parseFloat(total.toFixed(2));
+        return { ...state, total, amount };
+    }
+    if (action.type === 'LOADING') {
+        return { ...state, loading: true };
+    }
+    if (action.type === 'DISPLAY_ITEMS') {
+        return { ...state, loading: false, cart: action.payload };
     }
     return state;
 }
